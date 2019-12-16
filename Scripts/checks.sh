9740275b4ece4271117ad043309e4c0ee2ac3f44
Shewa00 <47750099+Shewa00@users.noreply.github.com>
2019-12-16 12:39:13 +0300

    checks

---
+++ Scripts/checks.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+
+files=`find . -name "*.cpp" -or -name "*.hpp" -or -name ".h" | grep -v "./tools/*" | grep -v "PicoSHA2"`
+filter=-build/c++11,-runtime/references,-whitespace/braces,-whitespace/indent,-whitespace/comments,-build/include_order,-runtime/threadsafe_fn
+echo $files | xargs cpplint --filter=$filter
