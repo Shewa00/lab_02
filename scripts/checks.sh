3b55b047e4994a910a143250a39456024a25ac5d
Shewa00 <KiraKryt63@mail.ru>
2019-12-17 09:14:07 +0300

    upd

--- scripts/checks.sh
+++ scripts/checks.sh
@@ -2,5 +2,5 @@
 set -e
 
 files=`find . -name "*.cpp" -or -name "*.hpp" -or -name ".h" | grep -v "./tools/*" | grep -v "PicoSHA2"`
-filter=-build/c++11,-runtime/references,-whitespace/braces,-whitespace/indent,-whitespace/comments,-build/include_order,-runtime/threadsafe_fn
+filter=-build/c++11,-runtime/references,-whitespace/braces,-whitespace/indent,-whitespace/comments,-build/include_order,-runtime/threadsafe_fn,-build/include
 echo $files | xargs cpplint --filter=$filter
